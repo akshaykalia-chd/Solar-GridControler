2a78a0c12eb2d1a0ccf515a7dde9624f3157edec
Akshay Kalia <akalia@vmware.com>
2021-12-01 21:29:32 +0530

    Adds LCD_V8 code

--- Arduino/Ardruino_timer/Timer_LCD/timer_LCD_V20/Sys_Prams.ino
+++ Arduino/Ardruino_timer/Timer_LCD/timer_LCD_V20/Sys_Prams.ino
@@ -1,12 +1,12 @@
 //*******************************************************
-//Function to setup System Voltage  
+//Function to setup System Voltage
 //EPROM Address 21
 //*******************************************************
 void set_sysvolts()
 {
-  byte sysvolts = EEPROM.read(21); 
-  sysvolts = update_Tcom("System Volts",sysvolts,255);
-  EEPROM.write(21,sysvolts);
+  byte sysvolts = EEPROM.read(21);
+  sysvolts = update_Tcom("System Volts", sysvolts, 255);
+  EEPROM.write(21, sysvolts);
 }
 //*******************************************************
 //Function to setup System Amps
@@ -14,9 +14,9 @@ void set_sysvolts()
 //*******************************************************
 void set_sysamps()
 {
-  byte sysamps = EEPROM.read(22); 
-  sysamps = update_Tcom("System Amps",sysamps,255);
-  EEPROM.write(22,sysamps);
+  byte sysamps = EEPROM.read(22);
+  sysamps = update_Tcom("System Amps", sysamps, 255);
+  EEPROM.write(22, sysamps);
 }
 //*******************************************************
 //Function to setup Data Logging
@@ -24,7 +24,7 @@ void set_sysamps()
 //*******************************************************
 void set_loger()
 {
-  byte loger = EEPROM.read(29); 
-  loger = update_Tcom("Loger",loger,2);
-  EEPROM.write(29,loger);
+  byte loger = EEPROM.read(29);
+  loger = update_Tcom("Loger", loger, 2);
+  EEPROM.write(29, loger);
 }
